e585a2d2acad639424e75367982eb1775da97ce2
ryzregz <morryzregz100@gmail.com>
2023-08-15 16:34:21 +0300

    ädding new changes on 15th August 2023

--- EehPae/util/Extensions.swift
+++ EehPae/util/Extensions.swift
@@ -80,6 +80,25 @@ extension UITextField {
         let validator = VaildatorFactory.validatorFor(type: validationType)
         return try validator.validated(self.text!)
     }
+    
+    func setPadding(left: CGFloat, right: CGFloat? = nil) {
+        setLeftPadding(left)
+        if let rightPadding = right {
+            setRightPadding(rightPadding)
+        }
+    }
+
+    private func setLeftPadding(_ padding: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: padding, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+
+    private func setRightPadding(_ padding: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: padding, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
 }
 
 extension URLComponents {
